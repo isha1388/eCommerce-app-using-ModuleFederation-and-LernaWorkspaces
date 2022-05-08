90a649c4e3315e58b2580e0a8f31071c079abbe9
Isha1388 <isha.1388.gmail.com>
2022-05-08 23:21:50 +0530

    Updating the READMEs

--- cart/src/components/ShoppingCart.tsx
+++ cart/src/components/ShoppingCart.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { useQuery } from "react-query";
-import { Product } from "@frontend-starter/sdk";
-import { Api } from "@frontend-starter/sdk";
 import { GridContainer, makeStyles, Typography } from "@frontend-starter/ui-components";
 
 const useStyles = makeStyles((theme) => ({
@@ -11,22 +8,8 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-// type ProductsResp = {
-//   data: Product[]
-// };
-
-// const getProducts = async (): Promise<ProductsResp> => {
-//   return await Api.getRecommendedProducts();
-// }
-
 const ShoppingCart = () => {
-  // const { data, isLoading, error } = useQuery<ProductsResp>(
-  //   "getProducts",
-  //   getProducts
-  // );
   const classes = useStyles();
-  // if (isLoading) return <div>Loading ...</div>;
-  // if (error) return <div>Something went wrong ...</div>;
   return <>
   <GridContainer className={classes.outline}>
     <Typography variant="h5" align="center">
